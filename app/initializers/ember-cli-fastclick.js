d3ebffa42cad14003c2ea9cf49eb0685e5b33c96
Moritz Lawitschka <me@moritzlawitschka.de>
2014-12-08 18:08:00 +0100

    Initialize FastClick after render

---
+++ app/initializers/ember-cli-fastclick.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+var EmberCliFastclickInitializer = {
+  name: 'fastclick',
+
+  initialize: function() {
+    Ember.run.schedule('afterRender', function() {
+      FastClick.attach(document.body)
+    });
+  }
+}
+
+export default EmberCliFastclickInitializer;
