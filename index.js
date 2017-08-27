156b4faa4e2237cdc8f9dce8d6c7f50dde52ea49
Brian Whitton <me@brianwhitton.com>
2017-08-27 18:43:07 -0400

    wrap fastclick lib for FastBoot compat

--- index.js
+++ index.js
@@ -4,13 +4,10 @@
 var path = require('path');
 var broccoliFunnel = require('broccoli-funnel');
 var broccoliMergeTrees = require('broccoli-merge-trees');
+var map = require('broccoli-stew').map;
 
 module.exports = {
   name: 'ember-cli-fastclick',
-
-  isEnabled: function() {
-    return !process.env.EMBER_CLI_FASTBOOT;
-  },
 
   included: function(app) {
     this._super.included(app);
@@ -23,10 +20,12 @@ module.exports = {
       trees.push(vendorTree);
     }
     var libPath = path.dirname(require.resolve('fastclick'));
-    trees.push(new broccoliFunnel(libPath, {
+    var fastclickLib = new broccoliFunnel(libPath, {
       destDir: 'fastclick',
       include: ['*']
-    }));
+    });
+    fastclickLib = map(fastclickLib, content => `if (typeof FastBoot === 'undefined') { ${content} }`);
+    trees.push(fastclickLib);
     return broccoliMergeTrees(trees);
   }
 };
